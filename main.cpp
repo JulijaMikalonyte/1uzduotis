631dec5291d311ed324458ae9c5c0ae6ca1a8931
JulijaMikalonyte <90048156+JulijaMikalonyte@users.noreply.github.com>
2021-09-17 18:32:52 +0300

    pirmos uzduoties papildymas

--- main.cpp
+++ main.cpp
@@ -14,19 +14,27 @@ int main(){
   int n;//studentu skaicius
   std::cin>>n;
   studentas grupe[n];//studentas-naudojamas duomenu tipas kuri patys apsirasem, tokio tipo turesim grupe su n nariu
-  for (int i=0; i<n; i++)//surenkam kiekvieno studento duomenis
+  float suma=0;//reikalinga apskaiciuoti vidurki
+  for (int i=0; i<n; i++)//for funkcija skirta duomenu apie studentus surinkimui
   {
-    std::cout<<"Iveskite "<<i+1<<"-jo studento varda:\n";
+    std::cout<<"Iveskite "<<i+1<<"-jo studento vardas:\n";
     std::cin>>grupe[i].Vardas;
     std::cout<<"Iveskite "<<i+1<<"-jo studento pavarde:\n";
     std::cin>>grupe[i].Pavarde;
-    for (int j=0; j<10; j++)//surenkam kiekvieno studento pazymius
+    suma=0;
+    for (int j=0; j<10; j++)//for funkcija skirta kiekvieno studento visu pazymiu surinkimui
     {
-      std::cout<<"Iveskite pazimi uz "<<j+1<<" namu darba:\n";
+      std::cout<<"Iveskite pazymi uz "<<j+1<<" namu darba:\n";
       std::cin>>grupe[i].nd[j];//pirmos gr 1-10 pazymai, antros ir t.t.
+      suma=suma+grupe[i].nd[j];//iskart skaiciuojame is pazymiu suma, suma + atitinkamas pazimys
     }
     std::cout<<"iveskite "<<i+1<<"-jo studento egzamino pazymi:\n";
     std::cin>>grupe[i].egzaminas;
-
+    grupe[i].galutinis=0.4*suma/10+0.6*grupe[i].egzaminas;
   }
+  std::cout<<std::setw(15)<<"Vardas"<<std::setw(15)<<"Pavarde"<<std::setw(20)<<"Galutinis pazymys\n"<<"-----------------------------------------------------\n";//atspausdiname antraste
+  for (auto &g:grupe)//foras skirtas duomenu isvedimui, automatiskai kintamajam priskiriama po viena elementa, kuris patalpintas masyve grupe; &-adresas (kad kiekviena kart nereiktu perrasinet kintamojo) i atitinkama elementa
+    
+    std::cout<<std::setw(15)<<g.Vardas<<std::setw(15)<<g.Pavarde<<std::setw(15)<<std::setprecision(2)<<g.galutinis<<std::endl;//isvedame ats, setw reiskia tarpus tarp zodziu, endl uzbaigia viska.
+  
 };
